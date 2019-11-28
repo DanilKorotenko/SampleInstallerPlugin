7a4162cb7001600b1a38c86b8828b226e957051a
Danil Korotenko <dkorotenko@gttb.com>
2019-11-28 09:05:13 -0800

    Check authorization

--- HelloGrantAccess/main.m
+++ HelloGrantAccess/main.m
@@ -11,22 +11,56 @@ int main(int argc, const char * argv[])
 {
     @autoreleasepool
     {
+        NSLog(@"argc: %d", argc);
+        
 		// Check if this process is launched with check full disk access key
 		if (argc > 1)
 		{
 			NSString *param = [NSString stringWithUTF8String:argv[1]];
 			if ([param isEqualToString:@"-c"])
 			{
-				int c = open("/Library/Application Support/com.apple.TCC/TCC.db", O_RDONLY);
+                NSString *path = nil;
 
-				if (c == -1 && (errno == EPERM || errno == EACCES))
-				{
-					return 1; // not authorized
-				}
-				else
-				{
-					return 0;
-				}
+                NSLog(@"enumerating thru Desktop");
+                path = NSHomeDirectory();
+                path = [path stringByAppendingPathComponent:@"Desktop"];
+                // Find the first any file in the user Library deirectory
+                NSDirectoryEnumerator *enumerator = [[NSFileManager defaultManager] enumeratorAtPath:path];
+                    
+                if (nil == [enumerator nextObject])
+                {
+                    NSLog(@"enumerator next object is nil");
+                    return 1;
+                }
+                    
+                NSString *filename = nil;
+                while (filename = [enumerator nextObject])
+                {
+                    NSLog(@"filename: %@", filename);
+
+                    BOOL isDirectory = NO;
+                    BOOL isExist = [[NSFileManager defaultManager] fileExistsAtPath:filename
+                        isDirectory:&isDirectory];
+                    NSLog(@"isExist:     %@", isExist ? @"YES" : @"NO");
+                    NSLog(@"isDirectory: %@", isDirectory ? @"YES" : @"NO");
+                    if (isExist && !isDirectory)
+                    {
+                        int c = open([filename UTF8String], O_RDONLY);
+
+                        if (c > 0)
+                        {
+                            return 0;
+                        }
+                        else if (errno == EPERM || errno == EACCES)
+                        {
+                            return 1; // not authorized
+                        }
+                        else
+                        {
+                            return errno;
+                        }
+                    }
+                }
 			}
 		}
     }
